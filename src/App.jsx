f932de7485fa57a326e4b6f0e7f9359ece511bc9
Pedro Alcoba <pedralcg@gmail.com>
2025-07-15 10:37:41 +0200

    Part05d - Ejer 5.17 Bloglist Playwright, paso 1
    Añadido data-testid

--- src/App.jsx
+++ src/App.jsx
@@ -192,6 +192,7 @@ const App = () => {
             type="text"
             value={username}
             name="Username"
+            data-testid="username-input"
             onChange={({ target }) => setUsername(target.value)}
           />
         </div>
@@ -201,6 +202,7 @@ const App = () => {
             type="password"
             value={password}
             name="Password"
+            data-testid="password-input"
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
